c86d590014bc0bc7707e553cc5efa3096832ad47
Emily G <aspentree79@gmail.com>
2019-07-28 18:14:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -29,5 +29,6 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-array.find { |cheese| cheese.include? (cheese_types)}
+array.find do  |cheese|
+  cheese_types.include? (cheese)
 end
